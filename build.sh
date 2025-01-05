f3778d8b2cd7832cc6deb091354958f3f859404a
Timo Hanke <t@timohanke.org>
2025-01-05 13:53:29 +0100

    Add wasm-opt
    Add binaryen/wasm-opt to the base image
    Run wasm-opt in build script
    Need a timeout because wasm-opt can go into an infinte loop, but
    still produces a correct result.
    Remove unnecessary files

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-OUT=out/out_$(uname -s)_$(uname -m).wasm
-moc src/main.mo -o $OUT -c
-if [ "$compress" == "yes" ] || [ "$compress" == "y" ]; then
-  gzip -nf $OUT
-  sha256sum $OUT.gz
-else
-  sha256sum $OUT
-fi
+OUT=out/out_$(uname -s)_$(uname -m)
+moc src/main.mo -c -o $OUT.wasm
+timeout 3s wasm-opt -O2 $OUT.wasm -o $OUT-opt.wasm
+sha256sum $OUT-opt.wasm
+[ -f $OUT.wasm ] && rm $OUT.wasm
